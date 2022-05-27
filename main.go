2b8e6322290809c3e7e722665542a38ac286e61a
Oleh Borysevych <ogogoleg@gmail.com>
2022-05-28 02:54:01 +0300

    updated structure

--- main.go
+++ main.go
@@ -4,11 +4,10 @@ import (
 	//"fmt"
 	//problem_1 "ogogoleg/leetgo/problems/1"
 	"fmt"
-	problem_13 "ogogoleg/leetgo/problems/13"
+	problem_13 "leetgo/problems/13"
 )
 
 func main() {
 	//fmt.Println(problems.TwoSums([]int{1, 2}, 1))
-
 	fmt.Printf("Result = %d", problem_13.RomanToInt("XIII"))
 }
